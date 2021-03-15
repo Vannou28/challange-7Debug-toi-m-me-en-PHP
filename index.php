124223b0ec2e1a59d097311964a550fc9b3456de
aurelien <79804905+Vannou28@users.noreply.github.com>
2021-03-15 21:07:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+];
+
+$characters [] = ["Aurelien" => ["city"=> "Outrouville","weapon"=> "moissonneuse"]];
+
+var_dump($characters);
+
+
+?>
